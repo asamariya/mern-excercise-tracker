ba1411c4d8b8ae6e8a872ea95889a21f80b9b31a
Abhishek Samariya <asamariya@gmail.com>
2020-01-17 16:05:15 +1100

    Uncommented window location after creating exercise

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -58,7 +58,7 @@ const CreateExercise = () => {
       .post('http://localhost:5000/exercises/add', exercise)
       .then(res => console.log(res.data));
 
-    // window.location = '/';
+    window.location = '/';
   };
 
   return (
